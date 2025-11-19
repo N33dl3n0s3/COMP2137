b33cb6d88ee3272b29f5010e9add609e8d147425
N33dl3n0s3 <aaronthistle@hotmail.com>
2025-11-19 11:10:38 -0500

    realized that I wanted a file where I accidentally made a directory

--- assignment2.sh
+++ assignment2.sh
@@ -204,7 +204,7 @@ for userName in "${userList[@]}"; do
 	for keyType in "${keyAlgorithm[@]}"; do
 		keyDirectory="$userSshDirectory/id_$keyType"
 		if [ ! -f "$keyDirectory" ]; then
-			mkdir -p "$userAuthKeys"
+			touch "$userAuthKeys"
 			echo "creating $keyType key for $userName"
 			sudo -u "$userName" ssh-keygen -t "$keyType" -f "$keyDirectory" -N "" -q
 			echo "Generated $keyType key for $userName"
